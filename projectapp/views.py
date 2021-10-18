cca4ea2c14dc7cc15815cf7432f8c457602a8bcb
dongzoor <dongjoo92@gmail.com>
2021-10-19 01:59:32 +0900

    django course 41 commit

--- projectapp/views.py
+++ projectapp/views.py
@@ -1,17 +1,20 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic.list import MultipleObjectMixin
 
+from articleapp.models import Article
 from projectapp.decorators import project_ownership_required
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 
 
-@method_decorator(project_ownership_required, 'post')
-@method_decorator(project_ownership_required, 'get')
+@method_decorator(login_required, 'post')
+@method_decorator(login_required, 'get')
 class ProjectCreateView(CreateView):
     model = Project
     form_class = ProjectCreationForm
@@ -21,10 +24,16 @@ class ProjectCreateView(CreateView):
         return reverse('projectapp:detail', kwargs={'pk': self.object.pk})
 
 
-class ProjectDetailView(DetailView):
+class ProjectDetailView(DetailView, MultipleObjectMixin):
     model = Project
     context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
+
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(project=self.get_object())
+        return super(ProjectDetailView, self).get_context_data(object_list=object_list, **kwargs)
 
 
 class ProjectListView(ListView):
