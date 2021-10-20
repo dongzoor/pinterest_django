e89580455d2ebf2db32733157454ec42b1f96da0
dongzoor <dongjoo92@gmail.com>
2021-10-20 14:31:01 +0900

    django course 45 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGIN_REDIRECT_URL = reverse_lazy('home')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 
 MEDIA_URL = '/media/'
